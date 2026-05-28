9365bf8bef47f9a598e7e7b0bb17345c25ca98d8
Nick <nicktelindert@protonmail.com>
2026-05-28 18:31:15 +0200

    Add Importer test and make sure it does not only test the success path

--- tests/Unit/ImporterTest.php
+++ tests/Unit/ImporterTest.php
@@ -53,4 +53,76 @@ class ImporterTest extends TestCase
         // Assert
         $this->assertEquals(2, $result);
     }
+
+    public function test_it_returns_zero_on_api_error_status(): void
+    {
+        // Arrange
+        $mockRepository = $this->createMock(ProductRepository::class);
+        $mockClient = $this->createMock(ClientInterface::class);
+
+        $mockClient->method('request')
+            ->willReturn(new Response(404));
+
+        $importer = new Importer($mockRepository, $mockClient);
+
+        // Act
+        $result = $importer->import();
+
+        // Assert
+        $this->assertEquals(0, $result);
+    }
+
+    public function test_it_returns_zero_on_invalid_json_payload(): void
+    {
+        // Arrange
+        $mockRepository = $this->createMock(ProductRepository::class);
+        $mockClient = $this->createMock(ClientInterface::class);
+
+        $mockClient->method('request')
+            ->willReturn(new Response(200, [], 'invalid-json'));
+
+        $importer = new Importer($mockRepository, $mockClient);
+
+        // Act
+        $result = $importer->import();
+
+        // Assert
+        $this->assertEquals(0, $result);
+    }
+
+    public function test_it_returns_zero_when_products_key_is_missing(): void
+    {
+        // Arrange
+        $mockRepository = $this->createMock(ProductRepository::class);
+        $mockClient = $this->createMock(ClientInterface::class);
+
+        $mockClient->method('request')
+            ->willReturn(new Response(200, [], json_encode(['foo' => 'bar'])));
+
+        $importer = new Importer($mockRepository, $mockClient);
+
+        // Act
+        $result = $importer->import();
+
+        // Assert
+        $this->assertEquals(0, $result);
+    }
+
+    public function test_it_returns_zero_on_client_exception(): void
+    {
+        // Arrange
+        $mockRepository = $this->createMock(ProductRepository::class);
+        $mockClient = $this->createMock(ClientInterface::class);
+
+        $mockClient->method('request')
+            ->willThrowException(new \Exception('Connection failed'));
+
+        $importer = new Importer($mockRepository, $mockClient);
+
+        // Act
+        $result = $importer->import();
+
+        // Assert
+        $this->assertEquals(0, $result);
+    }
 }
